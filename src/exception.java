fa5deb85bbe3c7773d516c01645843ae07532a8c
Abinesh S <abinesh781845@gmail.com>
2026-07-24 14:08:25 +0530

    throws

--- src/exception.java
+++ src/exception.java
@@ -1,7 +1,26 @@
+
 import java.util.*;
 public class exception {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),a[]=new int[n],sum=0;
+        for(int i=0;i<n;i++) {
+            a[i]=s.nextInt();
+        }
+
+        try {for(int  i:a) {
+
+            if(i<0)
+                throw new Exception("Invalid no."+i);
+            sum+=i;
+        }
+
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("sum:"+sum);
+        }
 
     }
+
 }
