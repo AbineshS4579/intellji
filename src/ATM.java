05ff340e47cc0cbededccbb3b1cfee24ab124fde
Abinesh S <abinesh781845@gmail.com>
2026-07-26 23:44:43 +0530

    atm machine udated

--- src/ATM.java
+++ src/ATM.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 class bank{
-    private int pin =1207,balance=10000;
+    private int pin,balance;
+    bank(int pin,int balance){
+        this.pin=pin;
+        this.balance=balance;
+    }
     int banks(){
        return pin;
     }
@@ -9,7 +13,7 @@ class bank{
     }
     void display(int w)throws Exception{
         if(balance<w)
-            throw new Exception("Invalid balance");
+            throw new Exception("Invalid balance.....");
         balance-=w;
     }
     int show(){
@@ -20,28 +24,37 @@ class bank{
         public static void main(String[] args){
             Scanner s=new Scanner(System.in);
             try{
-                int d=2;
+                System.out.print("Set a ATM pin:");
+                int pt=s.nextInt();
+                System.out.print("Enter the initial depositing amount:");
+                int ib=s.nextInt();
 
-                bank o = new bank();
+                bank o = new bank(pt,ib);
                 boolean l=true;
+                int d=2,y=0;
+                do {
+
                 while(l) {
                     System.out.print("Enter ATM pin:");
                     int p = s.nextInt();
                     if(o.banks()==p){
-                        l=false;
-                        System.out.println("pin is correct");
+                        System.out.println("pin is correct...");
+                        d=2;
+                        break;
                     }
                     else if(d>0){
                         System.out.println("please enter vailed pin you have only "+d+" chance");
                         d--;
                     }
                     else {
-                        System.out.println("your account is locked due invalid pin entered");
+                        System.out.println("your account is locked due invalid pin entered.....");
                         return ;
                     }
                 }
-                    System.out.print("1.Deposite\n2.Withdraw\n3.check Balance\n Enter the what the process you need:");
+
+                    System.out.print("1.Deposite\n2.Withdraw\n3.check Balance\nOther wise Enter :0 \nEnter the what the process you need:");
                     int x = s.nextInt();
+                    y++;
                     switch (x) {
                         case 1:
                             System.out.print("Enter Deposit amount:");
@@ -58,7 +71,12 @@ class bank{
                         case 3:
                             System.out.println("Balance:" + o.show());
                             break;
+                        default:
+                            return;
+
                     }
+                }while(y<5);
+                System.out.println("Your limit is over....");
 
             }catch(Exception e){
                 System.out.println(e.getMessage());
