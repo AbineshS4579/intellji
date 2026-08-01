d86f9b4207b164d0829d7e1496e15e9807629184
Abinesh S <abinesh781845@gmail.com>
2026-08-01 09:38:13 +0530

    list node1

--- src/node.java
+++ src/node.java
@@ -7,7 +7,7 @@ class ListNode{
     }
 }
 public class node {
-    static ListNode del(ListNode h,int val,int pos){
+    static ListNode ins(ListNode h,int val,int pos){
         ListNode ne=new ListNode(val);
         if(pos==1)
         {
@@ -25,12 +25,33 @@ public class node {
         t.next=ne;
         return h;
     }
+    static ListNode del(ListNode h,int pos){
+        if(pos==1){
+            h=h.next;
+            return h;
+        }
+        ListNode t=h;
+        for(int i=1;i<pos-1&&t.next!=null;i++){
+            t=t.next;
+        }
+        if(t.next==null){
+            System.out.println("not del");
+            return h;
+        }
+        t.next=t.next.next;
+        return h;
+    }
     public static void main(String[] args){
         ListNode n=new ListNode(10);
         n.next=new ListNode(20);
         n.next.next=new ListNode(30);
         //ListNode h=n;
-        del(n,34,3);
+        n=ins(n,34,3);
+//        while(n!=null){
+//            System.out.println(n.val+" ");
+//            n=n.next;
+//        }
+        n=del(n,2);
         while(n!=null){
             System.out.println(n.val+" ");
             n=n.next;
