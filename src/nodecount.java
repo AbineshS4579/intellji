e25883719fd7a778a6b54ff8a2d2274b4740370c
Abinesh S <abinesh781845@gmail.com>
2026-08-01 10:40:39 +0530

    listnode del from last

--- src/nodecount.java
+++ src/nodecount.java
@@ -1,30 +1,4 @@
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int val){
-        this.val=val;
-        this.next=null;
-    }
-}
-public class node {
-    static ListNode ins(ListNode h,int val,int pos){
-        ListNode ne=new ListNode(val);
-        if(pos==1)
-        {
-           ne.next=h;
-           return ne;
-        }
-        ListNode t=h;
-        for(int i=1;i<pos-1&&t!=null;i++)
-            t=t.next;
-        if(t==null) {
-            System.out.println("Invalid");
-            return h;
-        }
-        ne.next=t.next;
-        t.next=ne;
-        return h;
-    }
+public class nodecount {
     static ListNode del(ListNode h,int pos){
         if(pos==1){
             h=h.next;
@@ -52,13 +26,8 @@ public class node {
             te=te.next;
         }
         System.out.println(c);
-        //ListNode h=n;
-        n=ins(n,34,3);
-//        while(n!=null){
-//            System.out.println(n.val+" ");
-//            n=n.next;
-//        }
-        n=del(n,2);
+        int y=c-2+1;
+        n=del(n,y);
         while(n!=null){
             System.out.println(n.val+" ");
             n=n.next;
