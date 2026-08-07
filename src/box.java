5a8f029a30a27162122e0b55dc7c140f129137a0
Abinesh S <abinesh781845@gmail.com>
2026-08-07 20:04:55 +0530

    generic

---
+++ src/box.java
@@ -0,0 +1,14 @@
+public class box<t,u> {
+    t it;
+    u ht;
+    void getof(t it,u ht){
+        this.it=it;
+        this.ht=ht;
+    }
+    t putof(){
+        return it;
+    }
+    u putoff(){
+        return ht;
+    }
+}
---
+++ src/genric.java
@@ -0,0 +1,7 @@
+public class genric {
+    public static void main(String[] args){
+        box<Integer,String> l=new box<>();
+        l.getof(23,"String");
+        System.out.println(l.putof()+" "+l.putoff());
+    }
+}
